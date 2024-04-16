b1816c78007132ffa0daaeb1c465e5b239078ea4
Hugh Donkin <49598076+hsdonkin@users.noreply.github.com>
2024-04-16 13:25:58 -0700

    make list into a class

--- src/list.js
+++ src/list.js
@@ -1,125 +1,111 @@
-'use strict';
-
-var List = function (component) {
-  this.component = component;
-  this.items = [];
-  this.active = 0;
-  this.wrapper = document.createElement('div');
-  this.wrapper.className = 'suggestions-wrapper';
-  this.element = document.createElement('ul');
-  this.element.className = 'suggestions';
-  this.wrapper.appendChild(this.element);
-
-  // selectingListItem is set to true in the time between the mousedown and mouseup when clicking an item in the list
-  // mousedown on a list item will cause the input to blur which normally hides the list, so this flag is used to keep
-  // the list open until the mouseup
-  this.selectingListItem = false;
-
-  component.el.parentNode.insertBefore(this.wrapper, component.el.nextSibling);
-  return this;
-};
-
-List.prototype.show = function () {
-  this.element.style.display = 'block';
-};
-
-List.prototype.hide = function () {
-  this.element.style.display = 'none';
-};
-
-List.prototype.add = function (item) {
-  this.items.push(item);
-};
-
-List.prototype.clear = function () {
-  this.items = [];
-  this.active = 0;
-};
-
-List.prototype.isEmpty = function () {
-  return !this.items.length;
-};
-
-List.prototype.isVisible = function () {
-  return this.element.style.display === 'block';
-};
-
-List.prototype.draw = function () {
-  this.element.innerHTML = '';
-
-  if (this.items.length === 0) {
-    this.hide();
-    return;
+class List {
+  constructor(component) {
+    this.component = component;
+    this.items = [];
+    this.active = 0;
+    this.wrapper = document.createElement('div');
+    this.wrapper.className = 'suggestions-wrapper';
+    this.element = document.createElement('ul');
+    this.element.className = 'suggestions';
+    this.wrapper.appendChild(this.element);
+    this.selectingListItem = false;
+    component.el.parentNode.insertBefore(this.wrapper, component.el.nextSibling);
   }
 
-  for (var i = 0; i < this.items.length; i++) {
-    this.drawItem(this.items[i], this.active === i);
+  show() {
+    this.element.style.display = 'block';
   }
 
-  this.show();
-};
-
-List.prototype.drawItem = function (item, active) {
-  var li = document.createElement('li'),
-    a = document.createElement('a');
-
-  const id = 'suggestion-' + this.items.indexOf(item);
-
-  li.setAttribute('id', id);
-
-  if (active) {
-    this.element.setAttribute('aria-activedescendant', id);
-    li.className += ' active';
-    li.setAttribute('aria-selected', 'true');
+  hide() {
+    this.element.style.display = 'none';
   }
 
-  a.innerHTML = item.string;
+  add(item) {
+    this.items.push(item);
+  }
 
-  li.appendChild(a);
-  this.element.appendChild(li);
+  clear() {
+    this.items = [];
+    this.active = 0;
+  }
 
-  li.addEventListener(
-    'mousedown',
-    function () {
+  isEmpty() {
+    return !this.items.length;
+  }
+
+  isVisible() {
+    return this.element.style.display === 'block';
+  }
+
+  draw() {
+    this.element.innerHTML = '';
+
+    if (this.items.length === 0) {
+      this.hide();
+      return;
+    }
+
+    for (let i = 0; i < this.items.length; i++) {
+      this.drawItem(this.items[i], this.active === i);
+    }
+
+    this.show();
+  }
+
+  drawItem(item, active) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const id = 'suggestion-' + this.items.indexOf(item);
+    console.log(this.component.el);
+
+    li.setAttribute('id', id);
+
+    if (active) {
+      this.component.el.setAttribute('aria-activedescendant', id);
+      li.className += ' active';
+      li.setAttribute('aria-selected', 'true');
+    }
+
+    a.innerHTML = item.string;
+
+    li.appendChild(a);
+    this.element.appendChild(li);
+
+    li.addEventListener('mousedown', () => {
       this.selectingListItem = true;
-    }.bind(this)
-  );
+    });
 
-  li.addEventListener(
-    'mouseup',
-    function () {
-      this.handleMouseUp.call(this, item);
-    }.bind(this)
-  );
-};
+    li.addEventListener('mouseup', () => {
+      this.handleMouseUp(item);
+    });
+  }
 
-List.prototype.handleMouseUp = function (item) {
-  this.selectingListItem = false;
-  this.component.value(item.original);
-  this.clear();
-  this.draw();
-};
+  handleMouseUp(item) {
+    this.selectingListItem = false;
+    this.component.value(item.original);
+    this.clear();
+    this.draw();
+  }
 
-List.prototype.move = function (index) {
-  this.active = index;
-  this.draw();
-};
+  move(index) {
+    this.active = index;
+    this.draw();
+  }
 
-List.prototype.previous = function () {
-  this.move(this.active === 0 ? this.items.length - 1 : this.active - 1);
-};
+  previous() {
+    this.move(this.active === 0 ? this.items.length - 1 : this.active - 1);
+  }
 
-List.prototype.next = function () {
-  this.move(this.active === this.items.length - 1 ? 0 : this.active + 1);
-};
+  next() {
+    this.move(this.active === this.items.length - 1 ? 0 : this.active + 1);
+  }
 
-List.prototype.drawError = function (msg) {
-  var li = document.createElement('li');
-
-  li.innerHTML = msg;
-
-  this.element.appendChild(li);
-  this.show();
-};
+  drawError(msg) {
+    const li = document.createElement('li');
+    li.innerHTML = msg;
+    this.element.appendChild(li);
+    this.show();
+  }
+}
 
 module.exports = List;
